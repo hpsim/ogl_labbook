9f66b71bcbeb15599428db17dfb0078badf35f8e
Gregor Olenik <go@hpsim.de>
2022-05-20 19:56:43 +0200

    add submit script

---
+++ assets/lidDrivenCavity3D/HoreKa/submit_cases.sh
@@ -0,0 +1,5 @@
+export OBR_SELECT=100,simple;export OBR_SLURM_ARGS=--mem=243200mb;labbook execute --pipeline slurm_run_gpu --case lidDrivenCavity&
+export OBR_SELECT=200,simple;export OBR_SLURM_ARGS=--mem=243200mb;labbook execute --pipeline slurm_run_gpu --case lidDrivenCavity&
+export OBR_SELECT=300,simple;export OBR_SLURM_ARGS=--mem=243200mb;labbook execute --pipeline slurm_run_gpu --case lidDrivenCavity&
+export OBR_SELECT=400,simple;export OBR_SLURM_ARGS=--mem=243200mb;labbook execute --pipeline slurm_run_gpu --case lidDrivenCavity&
+export OBR_SELECT=500,simple;export OBR_SLURM_ARGS=--mem=243200mb;labbook execute --pipeline slurm_run_gpu --case lidDrivenCavity&
